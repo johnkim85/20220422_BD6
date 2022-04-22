66df7921f96814d27359f7de32e54acea454d4c3
johnkim85 <johnkim85@naver.com>
2022-04-22 12:26:27 +0900

    1에서 100까지 더하기
    업로드

---
+++ OneToHundred.java
@@ -0,0 +1,13 @@
+package Chapter05;
+
+public class OneToHundred {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int total=0;
+		for(int i=1;i<=100;i++) {
+			total+=i;
+		}
+		System.out.println(total);
+	}
+}
